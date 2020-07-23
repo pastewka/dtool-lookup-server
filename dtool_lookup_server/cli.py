daf597a9c89d4b824a0f0bc1a8faae6ddce05813
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2020-07-23 15:38:55 +0200

    Revert "MAINT: Access token is already a string"
    This reverts commit d9141e84a9f37ad2f0369652ed8008099fc80b3b.

--- dtool_lookup_server/cli.py
+++ dtool_lookup_server/cli.py
@@ -175,7 +175,7 @@ def generate_token(username, last_forever):
         token = create_access_token(identity=username, expires_delta=False)
     else:
         token = create_access_token(identity=username)
-    click.secho(token)
+    click.secho(token.decode("utf-8"))
 
 
 @base_uri_cli.command(name="index")
